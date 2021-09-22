2b13458e66d9d68dff383ea63fd163085b1baca3
Alvina Waseem <alvinawaseem@Alvinas-Air.fios-router.home>
2021-09-22 16:48:38 -0400

    add 2 more problems

--- src/main/java/EularProblems.java
+++ src/main/java/EularProblems.java
@@ -79,5 +79,60 @@ public class EularProblems {
         }
         System.out.println("LargestFactory: "+largestFactor);
 
+         //Problem 4
+
+        /*A palindromic number reads the same both ways. The largest palindrome made from the
+        product of two 2-digit numbers is 9009 = 91 × 99.
+        Find the largest palindrome made from the product of two 3-digit numbers.
+        */
+        int value = 0;
+        for(int i = 100;i <=999;i++)
+        {
+            for(int j = i;j <=999;j++)
+            {
+                int value1 = i * j;
+                StringBuilder sb1 = new StringBuilder(""+value1);
+                String sb2 = ""+value1;
+                sb1.reverse();
+                if(sb2.equals(sb1.toString()) && value<value1) {
+                    value = value1;
+
+                }
+
+            }
+        }
+
+        System.out.println("Palindrome:" +value);
+
+        //Problem 5
+        /*2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+          What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+         */
+
+        long cc = 21;
+        while (1==1)
+        {
+            boolean all = true;
+            for (int i = 1; i < 21; i++)
+            {
+                all = true;
+                if ( cc % i != 0 )
+                {
+                    all = false;
+                    break;
+                }
+            }
+            if ( all )
+            {
+                System.out.println("smallest positive number: "+cc );
+                System.exit(0);
+            }
+
+            cc++;
+        }
     }
 }
+
+
+
+
